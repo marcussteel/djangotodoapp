a2f58e88bc5354b83a86421c7059d73e9665bf41
marcussteel <resetyourcoin21@gmail.com>
2022-09-08 21:38:14 +0300

    bootstrap and css is  added

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import home, todo_create
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
     path('', home, name='home'),
     path("add/", todo_create, name="add"),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from multiprocessing import context
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
@@ -8,14 +8,22 @@ from .forms import TodoForm
 
 def home(request):
     todos = Todo.objects.all()
+    form = TodoForm()
     context = {
-        "todos" : todos
+        "todos" : todos,
+        'form' : form
     }
+    
 
     return render(request, 'todo/home.html',context)
 
 def todo_create(request):
     form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('home')
 
     context = {
         "form" : form
