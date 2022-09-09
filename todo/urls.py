fa6ed17d1be111f3e5f7c88d8695ae1638f1750d
marcussteel <resetyourcoin21@gmail.com>
2022-09-09 09:56:17 +0300

    update is added

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, todo_create
+from .views import home, todo_create, todo_update
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,5 +7,6 @@ urlpatterns = [
 
     path('', home, name='home'),
     path("add/", todo_create, name="add"),
+    path("update/<int:id>", todo_update, name="update"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 
+from multiprocessing import context
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
@@ -30,3 +31,17 @@ def todo_create(request):
     }
     return render(request,"todo/todo_add.html", context)
 
+def todo_update(request,id):
+    todo=Todo.objects.get(id=id)
+    form=TodoForm(instance=todo)
+    if request.method == "POST":
+        form = TodoForm(request.POST,instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context={
+        'todo':todo,
+        'form':form
+    }
+    return render(request, 'todo/todo_update.html', context)
